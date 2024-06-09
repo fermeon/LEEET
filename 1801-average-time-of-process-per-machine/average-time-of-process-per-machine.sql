2cd93fc443cb70c3b59b0d1c82fe46de16aeedf7
fermeon <149055547+fermeon@users.noreply.github.com>
2024-06-09 12:05:29 +0530

    Time: 223 ms (96.91%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1801-average-time-of-process-per-machine/average-time-of-process-per-machine.sql
@@ -0,0 +1,13 @@
+# Write your MySQL query statement below
+# since in the question we have to deal with consicutive rows so 1st idea should be join
+# now if we want "join" we should check for the condition "ON" 
+# "ON" uses condition so we give the condition as per requirement
+# and mathematical function that we want to show in our answer should be written after "Select"
+select a1.machine_id
+, ROUND(AVG(a2.timestamp - a1.timestamp),3) as processing_time
+from Activity a1
+join Activity a2
+on a1.process_id=a2.process_id
+and a1.machine_id=a2.machine_id
+and a1.timestamp<a2.timestamp
+group by a1.machine_id;
