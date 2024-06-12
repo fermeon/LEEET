9210741e4a76e3d3935cca4a303d636fe9aedcfa
fermeon <149055547+fermeon@users.noreply.github.com>
2024-06-12 16:01:09 +0530

    Time: 279 ms (97.50%) | Memory: 0B (100.00%) - LeetSync

---
+++ 602-friend-requests-ii-who-has-the-most-friends/friend-requests-ii-who-has-the-most-friends.sql
@@ -0,0 +1,15 @@
+select id ,count(id) as num
+from ((select requester_id as id 
+from RequestAccepted)
+union all
+(select accepter_id as id 
+from RequestAccepted)
+) AS combined_ids
+group by id 
+order by num desc 
+ limit 1
+
+
+
+
+
